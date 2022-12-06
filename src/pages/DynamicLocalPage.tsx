fb6a315fb4ebc3945a20dceb2149b467b2bda7ed
RoxaneBurri <roxane.burri@etu.unige.ch>
2022-12-06 20:20:30 +0100

    feat: put 2 images one below the other

--- src/pages/DynamicLocalPage.tsx
+++ src/pages/DynamicLocalPage.tsx
@@ -1,43 +1,49 @@
 import { FC, useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 
-import { Box, Container, Text } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 
 import DisplayImage from '../components/DisplayImage';
 import DynamicLocal from '../components/DynamicLocal';
-import ImageSelect from '../components/ImageSelect';
 import imageLabels from '../data/image_label.json';
 import { CategoryData } from '../types';
 import { fetchImageData, splitKeywordsInCategories } from '../utils/imageData';
 
 const DynamicLocalPage: FC = () => {
-  const { t } = useTranslation();
+  const Image1 = Object.keys(imageLabels)[1];
+  const Image2 = Object.keys(imageLabels)[3];
 
-  const defaultImage = Object.keys(imageLabels)[0];
-  const [imageId, setImageId] = useState<string>(Object.keys(imageLabels)[0]);
-  const [imageCategories, setImageCategories] = useState<CategoryData>({});
+  const [imageId1] = useState<string>(Object.keys(imageLabels)[1]);
+  const [imageId2] = useState<string>(Object.keys(imageLabels)[3]);
+
+  const [imageCategories1, setImageCategories1] = useState<CategoryData>({});
+  const [imageCategories2, setImageCategories2] = useState<CategoryData>({});
 
   useEffect(
     () => {
-      fetchImageData(defaultImage, (imageData) =>
-        setImageCategories(splitKeywordsInCategories(imageData)),
+      fetchImageData(Image1, (imageData) =>
+        setImageCategories1(splitKeywordsInCategories(imageData)),
       );
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
   );
+
+  useEffect(
+    () => {
+      fetchImageData(Image2, (imageData) =>
+        setImageCategories2(splitKeywordsInCategories(imageData)),
+      );
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
   return (
     <Container>
-      <Box>
-        <Text>{t('Select an image')}</Text>
-        <ImageSelect
-          imageId={imageId}
-          setImageId={setImageId}
-          setImageCategories={setImageCategories}
-        />
-      </Box>
-      <DisplayImage imageId={imageId} mt={2} />
-      <DynamicLocal data={imageCategories} />
+      <DisplayImage imageId={imageId1} mt={2} />
+      <DynamicLocal data={imageCategories1} />
+      <DisplayImage imageId={imageId2} mt={2} />
+      <DynamicLocal data={imageCategories2} />
     </Container>
   );
 };
--- src/pages/SimpleLocalPage.tsx
+++ src/pages/SimpleLocalPage.tsx
@@ -1,23 +1,37 @@
 import { FC, useEffect, useState } from 'react';
 
-import { Box, Container } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 
 import DisplayImage from '../components/DisplayImage';
-import ImageSelect from '../components/ImageSelect';
 import SimpleLocal from '../components/SimpleLocal';
 import imageLabels from '../data/image_label.json';
 import { CategoryData } from '../types';
 import { fetchImageData, splitKeywordsInCategories } from '../utils/imageData';
 
 const SimpleLocalPage: FC = () => {
-  const defaultImage = Object.keys(imageLabels)[0];
-  const [imageId, setImageId] = useState<string>(Object.keys(imageLabels)[0]);
-  const [imageCategories, setImageCategories] = useState<CategoryData>({});
+  const Image1 = Object.keys(imageLabels)[1];
+  const Image2 = Object.keys(imageLabels)[3];
+
+  const [imageId1] = useState<string>(Object.keys(imageLabels)[1]);
+  const [imageId2] = useState<string>(Object.keys(imageLabels)[3]);
+
+  const [imageCategories1, setImageCategories1] = useState<CategoryData>({});
+  const [imageCategories2, setImageCategories2] = useState<CategoryData>({});
 
   useEffect(
     () => {
-      fetchImageData(defaultImage, (imageData) =>
-        setImageCategories(splitKeywordsInCategories(imageData)),
+      fetchImageData(Image1, (imageData) =>
+        setImageCategories1(splitKeywordsInCategories(imageData)),
+      );
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
+  useEffect(
+    () => {
+      fetchImageData(Image2, (imageData) =>
+        setImageCategories2(splitKeywordsInCategories(imageData)),
       );
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -26,15 +40,10 @@ const SimpleLocalPage: FC = () => {
 
   return (
     <Container w="100pw">
-      <Box>
-        <ImageSelect
-          imageId={imageId}
-          setImageId={setImageId}
-          setImageCategories={setImageCategories}
-        />
-      </Box>
-      <DisplayImage imageId={imageId} mt={2} />
-      <SimpleLocal data={imageCategories} />
+      <DisplayImage imageId={imageId1} mt={2} />
+      <SimpleLocal data={imageCategories1} />
+      <DisplayImage imageId={imageId2} mt={2} />
+      <SimpleLocal data={imageCategories2} />
     </Container>
   );
 };
