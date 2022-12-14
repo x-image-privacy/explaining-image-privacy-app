bd2bce307b56926350a825447f2a2d20bbc471c1
spaenleh <spaenleh@gmail.com>
2022-12-14 08:57:31 +0100

    feat: add google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import GA4React from 'ga-4-react';
+
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -15,6 +17,30 @@ import MainPage from './pages/MainPage';
 import SimpleLocalPage from './pages/SimpleLocalPage';
 import WordCloudLocalPage from './pages/WordCloudLocalPage';
 import theme from './theme';
+
+const { REACT_APP_GA = '' } = process.env;
+
+const ga4react = new GA4React(
+  REACT_APP_GA,
+  {
+    /* ga custom config, optional */
+  },
+  [
+    /* additional code, optional */
+  ],
+  5000 /* timeout, optional, defaults is 5000 */,
+);
+
+ga4react.initialize().then(
+  (ga4) => {
+    ga4.pageview('path');
+    ga4.gtag('event', 'pageview', 'path'); // or your custom gtag event
+  },
+  (err) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  },
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
